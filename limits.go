bc5c510dc1d8aaf3a20b7035537f7e547769aec0
Stephan Peijnik <speijnik@anexia-it.com>
2018-03-13 13:54:02 +0100

    Add missing docstrings for limits.go
    Signed-off-by: Stephan Peijnik <speijnik@anexia-it.com>

--- limits.go
+++ limits.go
@@ -18,23 +18,27 @@ type Limit struct {
 	hard *uint64
 }
 
+// SetHard sets the hard limit
 func (l *Limit) SetHard(limit uint64) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.hard = &limit
 }
 
+// GetHard retrieves the hard limit
 func (l *Limit) GetHard() (limit uint64) {
 	limit, _, _ = l.getValues()
 	return
 }
 
+// SetSoft sets the soft limit
 func (l *Limit) SetSoft(limit uint64) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.soft = &limit
 }
 
+// GetSoft retrieves the soft limit
 func (l *Limit) GetSoft() (limit uint64) {
 	_, limit, _ = l.getValues()
 	return
